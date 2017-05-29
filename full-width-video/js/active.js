565da6120361d181c3c6be062b06178bb999db49
saifurit <saifur703@gmail.com>
2017-05-29 06:00:40 +0600

    full-width-video

---
+++ full-width-video/js/active.js
@@ -0,0 +1,12 @@
+(function($){
+	
+	jQuery(document).ready(function($){
+		
+		// magnific popup for video
+		$('.video-play-btn').magnificPopup({
+			type: 'video',
+		});
+		
+	});
+	
+})(jQuery);
