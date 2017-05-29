fff2b54a6098f26341546caf3849bc6a5d3bbb4b
saifurit <saifur703@gmail.com>
2017-05-29 06:02:37 +0600

    exam-2.test

---
+++ exam-2.test/assets/js/active.js
@@ -0,0 +1,20 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		
+		
+		// magnific popup for video
+		$('.video_play').magnificPopup({
+			type: 'video',
+		});
+		
+		
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
