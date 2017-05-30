b2cf138b9f0229c8bc671431efc7ce3dce5c5b33
saifurit <saifur703@gmail.com>
2017-05-31 03:47:01 +0600

    special-carousel

---
+++ special-carousel/assets/js/active.js
@@ -0,0 +1,19 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		
+		$('.trailor_carousel').owlCarousel({
+			items: 1,
+			loop: true,
+			autoplay: true,
+		});
+		
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
