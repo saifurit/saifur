a48b2cf741d250eb2796bc6d923fb734a18d6946
saifurit <saifur703@gmail.com>
2017-06-01 07:33:36 +0600

    testimonial carusel

---
+++ testimonial carusel/assets/js/active.js
@@ -0,0 +1,17 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$('.testimonial_slide').owlCarousel({
+			items: 1,
+			loop: true,
+			autoplay: true,
+		});
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
