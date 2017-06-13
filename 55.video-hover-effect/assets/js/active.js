60571125e34408f60fbf69dd60d6613181f20eb8
saifurit <saifur703@gmail.com>
2017-06-13 21:21:25 +0600

    55.video-hover-effect

---
+++ 55.video-hover-effect/assets/js/active.js
@@ -0,0 +1,16 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		// magnific popup for video
+		$('.video-play-btn').magnificPopup({
+			type: 'video',
+		});
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
