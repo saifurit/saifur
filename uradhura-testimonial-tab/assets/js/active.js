f28d611f039d46b59084e8ad4635fea2d9277251
saifurit <saifur703@gmail.com>
2017-06-18 17:32:59 +0600

    uradhura-testimonial-tab

---
+++ uradhura-testimonial-tab/assets/js/active.js
@@ -0,0 +1,13 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$('.testimonial_author_name').perfectScrollbar();
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
