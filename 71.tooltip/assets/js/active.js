16f94d01f59a713118ca81220bcb0142389632cc
saifurit <saifur703@gmail.com>
2017-07-22 00:50:34 +0600

    71.tooltip

---
+++ 71.tooltip/assets/js/active.js
@@ -0,0 +1,14 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$('[data-toggle=tooltip]').tooltip();
+		$('[data-toggle=popover]').popover();
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
