d2ea0b1de87e14e04e70c0d3ff46943ea4102786
saifurit <saifur703@gmail.com>
2017-07-23 01:04:29 +0600

    counter-up

---
+++ counter-up/js/active.js
@@ -0,0 +1,11 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$('.counter_up').counterUp({
+            delay: 10,
+            time: 1000,
+        });
+	});
+	
+})(jQuery);
