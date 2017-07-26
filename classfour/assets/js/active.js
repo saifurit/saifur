b8e37207f5c8e59569a3d3c7df640f7f16760a99
shakil-hossain <Shakilhossain4386@gmail.com>
2017-07-26 14:50:57 +0600

    classfour

---
+++ classfour/assets/js/active.js
@@ -0,0 +1,22 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		
+        $('.our_team').owlCarousel({
+            items: 3,
+            loop: true,
+            margin: 30,
+            dots: true,
+            nav: false,
+            
+        });
+        
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
