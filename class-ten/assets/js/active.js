13a47f1e7f38c79f3ccfbda494dacb0ff4f6a269
shakil-hossain <Shakilhossain4386@gmail.com>
2017-08-02 00:48:28 +0600

    class-ten

---
+++ class-ten/assets/js/active.js
@@ -0,0 +1,19 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$(".home_slider").owlCarousel({
+            items:1,
+            loop:true,
+            autoplay:true,
+            nav:true,
+              navText:["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"]
+        });
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
