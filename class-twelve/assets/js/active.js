bd71d47482dfa5320f2e6455441cb6a9957b2544
shakil-hossain <Shakilhossain4386@gmail.com>
2017-08-04 15:05:59 +0600

    class-twelve

---
+++ class-twelve/assets/js/active.js
@@ -0,0 +1,15 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$('video-play-btn').modalVideo({
+            type:'video',
+        });
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
