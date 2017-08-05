f887a59e778c3ead65a2239c0dc0ad2ab53dad27
shakil-hossain <Shakilhossain4386@gmail.com>
2017-08-06 01:51:08 +0600

    class-13

---
+++ class-13/assets/js/active.js
@@ -0,0 +1,41 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+        var homepageSlides = $(".homepage_slides");
+        
+        homepageSlides.owlCarousel({
+            items:1,
+            loop:true,
+            nav:true,
+        });
+        
+		$('.video-play-btn').magnificPopup({
+            type:'video',
+        });
+	
+        homepageSlides.on('translated.owl.carousel', function(event) {
+            $(".welcome_text h4").addClass("animated zoomIn");
+            $(".welcome_text h1").addClass("animated jackInTheBox");
+        });
+        homepageSlides.on('translate.owl.carousel', function(event) {
+            $(".welcome_text h4").removeClass("animated zoomIn");
+            $(".welcome_text h1").removeClass("animated jackInTheBox");
+        });  
+        
+        
+        $(".video-bg").YTPlayer({
+            fitToBackground: true,
+            videoId: 'LSmgKRx5pBo'
+        });
+        
+        
+});
+        
+    });
+
+        jQuery(window).load(function(){
+
+        });
+	
+})(jQuery);
