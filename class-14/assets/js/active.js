d86126566597ace9df232d22b90dbd89408afa9a
shakil-hossain <Shakilhossain4386@gmail.com>
2017-08-06 23:17:48 +0600

    class-14

---
+++ class-14/assets/js/active.js
@@ -0,0 +1,15 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$(".portfolio_list").masonry({
+            horizontalOrder: true
+        });
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
