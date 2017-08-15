f198d19d78d3dfe827fa7c71b470f8e7b71bdc65
shakil-hossain <Shakilhossain4386@gmail.com>
2017-08-15 13:54:58 +0600

    class-22

---
+++ class-22/assets/js/active.js
@@ -0,0 +1,19 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$(".service_box").owlCarousel({
+            items:5,
+            nav:true,
+            loop:true,
+            dots:true,
+            navText:["<i class='fa fa-long-arrow-left'> <i class='fa fa-long-arrow-right'>"]
+        })
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
