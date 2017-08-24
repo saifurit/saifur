74c1c4c2088d47255649fc1ae0dd333d551a3a56
shakil-hossain <Shakilhossain4386@gmail.com>
2017-08-24 16:04:53 +0600

    landing_2

---
+++ landing_2/assets/js/active.js
@@ -0,0 +1,37 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		
+        $('.portfolio_list').masonry({
+             percentPosition: true,  
+        });
+        
+        $(".single_portfolio").hover(function(){
+            $(".single_portfolio .portfolio_hover h2").removeClass("animated slideInUp");
+            $(this).find(".portfolio_hover h2").addClass("animated slideInUp");
+        });
+        
+        $(".navbar_toogle").on('click', function(){
+            $(".off-canvas-menu").addClass("active");
+            $(".off_canvas_menu_overlay").addClass("active");
+        });
+        
+        $(".menu_cross i.fa, .off_canvas_menu_overlay").on('click', function(){
+            $(".off-canvas-menu").removeClass("active");
+            $(".off_canvas_menu_overlay").removeClass("active");
+        });
+        
+        
+        
+        
+        
+        
+	});
+	
+	
+	jQuery(window).load(function(){
+		
+	});
+	
+})(jQuery);
